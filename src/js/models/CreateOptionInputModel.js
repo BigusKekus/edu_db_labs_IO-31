19348776147e8ba2f7e81f9c0bfcc7151f9d41be
BigusKekus <misha.vihlaev@gmail.com>
2025-06-04 22:43:13 +0300

    Delete src/js directory

--- src/js/models/CreateOptionInputModel.js
+++
@@ -1,7 +0,0 @@
-export class CreateOptionInputModel {
-  constructor(question_id, option_text) {
-    this.question_id = question_id;
-    this.option_text = option_text;
-  }
-}
-export default CreateOptionInputModel;
--- src/js/models/CreateSurveyInputModel.js
+++
@@ -1,8 +0,0 @@
-export class CreateSurveyInputModel {
-  constructor(title, description, author_id) {
-    this.title = title;
-    this.description = description;
-    this.author_id = author_id;
-  }
-}
-export default CreateSurveyInputModel;
